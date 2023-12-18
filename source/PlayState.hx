70da393274bed828174d15054b2604b778dfcd6c
Oscar Cederberg <os7138ce-s@student.lu.se>
2023-12-18 12:50:54 +0100

    Format

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,15 +2,12 @@ package;
 
 import flixel.FlxState;
 
-class PlayState extends FlxState
-{
-	override public function create()
-	{
-		super.create();
-	}
+class PlayState extends FlxState {
+    override public function create() {
+        super.create();
+    }
 
-	override public function update(elapsed:Float)
-	{
-		super.update(elapsed);
-	}
+    override public function update(elapsed:Float) {
+        super.update(elapsed);
+    }
 }
