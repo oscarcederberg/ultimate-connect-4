fa9cb7a1aaa4373063a138d36c6bbee00e2a26a7
Oscar Cederberg <os7138ce-s@student.lu.se>
2024-01-18 13:49:54 +0100

    Check win condition

--- source/Board.hx
+++ source/Board.hx
@@ -31,8 +31,32 @@ class BoardSlot extends AttachableSprite {
         animation.play("empty");
     }
 
+    public function isPlayerPiece(player:BoardTurn):Bool {
+        switch type {
+        case Empty:
+            return false;
+        case Blue:
+            if (player == Blue) {
+                return true;
+            } else {
+                return false;
+            }
+        case Red:
+            if (player == Red) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
     public function getSlotType():BoardSlotType {
         return type;
+    }
+
+    public function resetSlot() {
+        this.type = Empty;
+        animation.play("empty");
     }
 
     public function setSlotType(turn:BoardTurn) {
@@ -64,6 +88,7 @@ class Board extends FlxSpriteGroup {
     private var slots:Array<Array<BoardSlot>> = [for (row in 0...ROWS) [for (col in 0...COLS) null]];
     private var columnHitboxes:Array<BoardColumnHitbox> = [for (row in 0...ROWS) null];
     private var turn:BoardTurn = Blue;
+    private var locked:Bool = false;
 
     override public function new(x:Float, y:Float) {
         super();
@@ -99,10 +124,114 @@ class Board extends FlxSpriteGroup {
         }
     }
 
+    private function checkWinCondition(turn:BoardTurn, row:Int, col:Int):Bool {
+        // check horizontally
+        var counter = 0;
+        for (offset in -3...4) {
+            var colToCheck = col + offset;
+            trace('row, col: (${row}, ${colToCheck})');
+
+            if (colToCheck < 0 || colToCheck >= COLS) {
+                counter = 0;
+                continue;
+            }
+
+            if (slots[row][colToCheck].isPlayerPiece(turn)) {
+                counter++;
+                if (counter >= 4) {
+                    return true;
+                }
+            } else {
+                counter = 0;
+            }
+        }
+
+        // check vertically
+        counter = 0;
+        for (offset in -3...4) {
+            var rowToCheck = row + offset;
+            trace('row, col: (${rowToCheck}, ${col})');
+
+            if (rowToCheck < 0 || rowToCheck >= ROWS) {
+                counter = 0;
+                continue;
+            }
+
+            if (slots[rowToCheck][col].isPlayerPiece(turn)) {
+                counter++;
+                if (counter >= 4) {
+                    return true;
+                }
+            } else {
+                counter = 0;
+            }
+        }
+
+        // check first diagonal
+        counter = 0;
+        for (offset in -3...4) {
+            var rowToCheck = row + offset;
+            var colToCheck = col + offset;
+            trace('row, col: (${rowToCheck}, ${colToCheck})');
+
+            if (rowToCheck < 0 || rowToCheck >= ROWS) {
+                counter = 0;
+                continue;
+            }
+
+            if (colToCheck < 0 || colToCheck >= COLS) {
+                counter = 0;
+                continue;
+            }
+
+            if (slots[rowToCheck][colToCheck].isPlayerPiece(turn)) {
+                counter++;
+                if (counter >= 4) {
+                    return true;
+                }
+            } else {
+                counter = 0;
+            }
+        }
+
+        // check second diagonal
+        counter = 0;
+        for (offset in -3...4) {
+            var rowToCheck = row - offset;
+            var colToCheck = col + offset;
+            trace('row, col: (${rowToCheck}, ${colToCheck})');
+
+            if (rowToCheck < 0 || rowToCheck >= ROWS) {
+                counter = 0;
+                continue;
+            }
+
+            if (colToCheck < 0 || colToCheck >= COLS) {
+                counter = 0;
+                continue;
+            }
+
+            if (slots[rowToCheck][colToCheck].isPlayerPiece(turn)) {
+                counter++;
+                if (counter >= 4) {
+                    return true;
+                }
+            } else {
+                counter = 0;
+            }
+        }
+
+        return false;
+    }
+
     private function makeMove(col:Int) {
         for (row in new ReverseIntIterator(ROWS, 0)) {
             if (slots[row][col].getSlotType() == Empty) {
                 slots[row][col].setSlotType(turn);
+                if (checkWinCondition(turn, row, col)) {
+                    trace("win!");
+                    resetBoard();
+                }
 
                 switch turn {
                 case Blue:
@@ -111,6 +240,14 @@ class Board extends FlxSpriteGroup {
                     this.turn = Blue;
                 }
                 break;
+            }
+        }
+    }
+
+    private function resetBoard() {
+        for (row in 0...ROWS) {
+            for (col in 0...COLS) {
+                slots[row][col].resetSlot();
             }
         }
     }
